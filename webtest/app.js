2a4ba77ca907a3b2ea869d34e1a57ca92a212ea6
sty0507 <styoung0507@gmail.com>
2022-06-02 22:02:47 +0900

    nodejs로 회원가입, 로그인 구현(DB 연동X)

--- webtest/app.js
+++ webtest/app.js
@@ -1,47 +1,68 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
-app.set("views", "./views");
+app.set("views", "./webtest/views");
 app.set("view engine", "ejs");
-app.use(express.static(`${__dirname}/public`));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extends : true}));
-
-app.use('/', require('./routes/home'))
-
+app.engine("html", require("ejs").renderFile);
+app.use(express.urlencoded({ extended: false }));
 
 module.exports = app;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+// =====================DB 역활=====================
+let primary_id = ""; 
+let primary_pw = "";
+let primary_name = "";
+// =================================================
+
+app.get("/home", (req, res) => {
+  res.render("home.html", { name: primary_name });
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.html");
 });
 
 app.get("/login", (req, res) => {
-  res.render("views/index");
+  res.render("index.html");
+});
+
+app.post("/register", (req, res) => {
+  primary_id = req.body.id;
+  primary_pw = req.body.pw;
+  let repw = req.body.rpw;
+  primary_name = req.body.name;
+  if (primary_pw === repw) { // 비밀번호 확인이 제대로 되었는지
+    console.log(primary_id);
+    console.log(primary_pw);
+    console.log(primary_name);
+    res.redirect("/login");
+  } else {
+    res.send(
+      "<script>alert('비밀번호가 일치하지 않습니다.'); window.location.replace('/register');</script>"
+    );
+  }
+});
+
+app.post("/login", (req, res) => {
+  let c_id = req.body.id;
+  let c_pw = req.body.pw;
+
+  console.log(c_id);
+  console.log(c_pw);
+
+  if (c_id === primary_id && c_pw === primary_pw) {
+    res.redirect("/home");
+  } else {
+    res.send(
+      "<script>alert('회원정보가 일치하지 않습니다.'); window.location.replace('/login');</script>"
+    );
+    // res.redirect("/login");
+  }
 });
 
 app.listen(3000, () => {
   console.log("서버 띄우기");
 });
 
-// function check(){
-//     var id = $('#id').val();
-//     var pw = $('#pw').val();
-//     var pw_c = $('#pw_c').val();
-//     var name = $('#name').val();
-
-//     if(id.length < 4) alert('id양식에 맞지 않습니다.')
-//     else if(pw.length < 4) alert('비밀번호는 4자 이상 입력하세요')
-//     else if(pw != pw_c) alert('비밀번호가 일치하지 않습니다.')
-//     else {
-//         $.ajax({
-//             url : '/signup',
-//             type : 'POST',
-//             data : {
-//                 pw : pw,
-//                 id : id,
-//                 name : name
-//             }
-//         })
-//     }
-// }
+// 참고 사이트 : https://cocoon1787.tistory.com/517
