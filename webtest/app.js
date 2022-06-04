6496bd01f6fda5616f15d34a2092918015300089
sty0507 <styoung0507@gmail.com>
2022-06-04 22:30:25 +0900

    값 가져오기 성공

--- webtest/app.js
+++ webtest/app.js
@@ -25,7 +25,7 @@ app.get("/login", (req, res) => {
   res.render("index.html");
 });
 
-app.post("/register", (req, res) => {
+app.post("/registerAF", (req, res) => {
   var params = [req.body.id, req.body.pw, req.body.name];
   var sql = "INSERT INTO PERSON VALUES(?, ?, ?)";
 
@@ -36,7 +36,7 @@ app.post("/register", (req, res) => {
       if (err) console.log("query is not excuted.\n" + err);
       else res.redirect("/register");
     });
-    res.redirect("/login");
+    res.redirect("/home");
   } else {
     res.send(
       "<script>alert('비밀번호가 일치하지 않습니다.'); window.location.replace('/register');</script>"
@@ -44,19 +44,47 @@ app.post("/register", (req, res) => {
   }
 });
 
-app.post("/login", (req, res) => {
+app.post("/loginAF", (req, res) => {
   let c_id = req.body.id;
   let c_pw = req.body.pw;
 
-  console.log(c_id);
-  console.log(c_pw);
+  //확인 해야 할것
+  // 1) 입력을 했는가
+  // 2) 아이디가 DB 상에 존재를 하는가
+  // 3) 입력한 아이디가 DB 상의 비밀번호와 일치하는가
 
-  var sql = "SELECT * FROM PERSON";
-  console.log(sql);
-  conn.query(sql, function (err, rows, fields) {
-    if (err) console.log("query is not excuted\n" + err);
-    else res.redirect("/login");
-  });
+  if (c_pw == "" || c_id == "") {
+    res.send(
+      "<script>alert('입력해주세요.'); window.location.replace('/login');</script>"
+    ); // 1)
+  } else {
+    console.log(c_id);
+    console.log(c_pw);
+
+    var sql_id = "SELECT pw FROM PERSON WHERE ID = " + '"' + c_id + '"'; // 아이디가 존재하는지 확인 후 그에 맞는 password 가져옴
+    var sql_pw = "SELECT name FROM PERSON WHERE ID = " + '"' + c_id + '"';
+    conn.query(sql_id, function (err, results) {
+      var result = results[0];
+
+      console.log(results);
+      console.log(result);
+
+      if (!results) {
+        // 2) result가 sql을 실행해서 가져옴. 만약 results가 비어있으면 일치하지가 않음(반환 형태가 [] 임.)
+        res.send(
+          "<script>alert('ID가 존재하지 않습니다.'); window.location.replace('/login');</script>"
+        );
+      }
+      else if (result.pw === c_pw) {
+        // 비밀번호 확인 3)
+        res.redirect("/home");
+      } else {
+        res.send(
+          "<script>alert('회원정보가 존재하지 않습니다.'); window.location.replace('/login');</script>"
+        );
+      }
+    });
+  }
 
   // if (c_id === primary_id && c_pw === primary_pw) {
   //   res.redirect("/home");
