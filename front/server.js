d71a6e7f992ee913aa2811b513f20e8b449ad5f0
sty0507 <styoung0507@gmail.com>
2022-07-05 12:17:55 +0900

    use cookie

--- front/server.js
+++ front/server.js
@@ -1,15 +1,21 @@
 var express = require("express");
 var cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const app = express();
+
 app.use(cookieParser());
 app.set("views", "./front/views");
 app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
+app.use(express.urlencoded({ extended: false }));
+
+module.exports = app;
 
 app.get("/", function (req, res) {
-  res.render("a.ejs");
+  res.render("a.html");
 });
 // 쿠키 생성
-app.post("/login", function (req, res) {
+app.post("/login", (req, res) => {
   res.cookie("id", req.body.p_id);
   res.cookie("pw", req.body.p_pw);
   res.redirect("/");
@@ -21,9 +27,12 @@ app.get("/state", function (req, res) {
 // 쿠키 업데이트
 app.get("/update", function (req, res) {
   res.cookie("FirstName", "Park");
+  res.redirect("/state");
 });
-app.get("logout", function (req, res) {
+// 쿠키 삭제
+app.get("/logout", function (req, res) {
   res.clearCookie("FirstName");
+  res.redirect("/state");
 });
 
 app.listen(8080, () => {
